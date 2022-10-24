6b72e91ddc9922001c04a074daf474ba5a411393
Vimal Kumar <mdjohnvimal@gmail.com>
2022-10-24 10:03:45 +0530

    Added lazy loading

--- authfe/src/App.js
+++ authfe/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Dashboard from './components/Dashboard';
-import React from 'react';
+// import SignUp from './components/SignUp';
+// import Dashboard from './components/Dashboard';
+import React, { lazy, Suspense } from 'react';
+const SignIn = lazy(() => import('./components/SignIn'))  // lazy loading - call the component only when its required
+const SignUp = lazy(() => import('./components/SignUp'))
+const Dashboard = lazy(() => import('./components/Dashboard'))
 const apiUrl = 'http://localhost:8000'
 
 export const BaseContext = React.createContext()
@@ -14,12 +16,15 @@ function App() {
     <BaseContext.Provider value={{apiUrl}}>
 
     <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+
       <Routes>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='*' element={<Navigate to='/signin'/>}/>
       </Routes>
+      </Suspense>
     </Router>
     </BaseContext.Provider>
   </div>
